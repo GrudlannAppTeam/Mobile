e79b83357ede997305da7a65460630746515a041
przemo2608 <przemo95732@gmail.com>
2021-01-17 22:44:38 +0100

    review form

--- views/ReviewForm.js
+++ views/ReviewForm.js
@@ -1,40 +1,88 @@
 import React from 'react';
 import axios from 'axios';
+import image from '../assets/home.png'
 import PropTypes from 'prop-types';
 import ReviewQuestion from './ReviewQuestion';
+import { StyleSheet, Text, View, ImageBackground, TextInput, Alert, ActivityIndicator } from 'react-native';
 
 
 class ReviewForm extends React.Component {
    
     state = {
         questions: [],
+        isLoading: false,
     };
     
      componentDidMount() {
-        axios.get('https://grudlann-app.herokuapp.com/api/questions/answers', { headers: { 'Authorization': `Bearer ${JSON.parse(localStorage.getItem('token')).value}`, 'Accept': '*/*' }})
-        .then((response) => {
-            console.log(response);
-            this.setState({
-                questions: response.data,
-            });
-        }).catch(err => console.log(err.message));
+        fetch('https://grudlann-app.herokuapp.com/api/questions/answers', { method: "GET", headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}`, 'Accept': '*/*' }})
+        .then((response) => response.json())
+        .then((json) => {
+            this.setState({ questions: json});
+            
+            
+        })
+        
+           .catch((error) => console.error(error))
+          .finally(() => {
+        this.setState({ isLoading: true });
+      });
+           
     }
   render() { 
-     const { questions } = this.state;  
+     const { questions, isLoading } = this.state;  
     return (
-        <div >
-            <div >
-                {questions.map(question => (
+        isLoading ? (
+        
+        <View style={styles.container}>
+             <ImageBackground source={image} style={styles.image}>
+                  <Text style={styles.text}>{console.log(questions)} </Text>
+                {questions.map(question => 
                     <ReviewQuestion
                         key={question.name}
                         name={question.name}
                         answers={question.answers}
                     />
-                ))}
-            </div>
-        </div>
+                )}
+            </ImageBackground>
+        </View>): null
     );
   };
 };
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%'
+    },
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center",
+        width: '100%'
+    },
+    text: {
+        color: "white",
+        fontSize: 13,
+        fontWeight: "bold",
+        textAlign: "center",
+        paddingHorizontal: 20,
+        marginBottom: 20,
+    },
+    input: {
+        borderColor: 'white',
+        borderWidth: 2,
+        marginHorizontal: 30,
+        color: 'white',
+        padding: 10,
+        marginTop: 20,
+    },
+    horizontal: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        padding: 10
+    }
+});
 
 export default ReviewForm;
--- views/ReviewQuestion.js
+++ views/ReviewQuestion.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import axios from 'axios';
+import image from '../assets/home.png'
 import PropTypes from 'prop-types';
 import AppButton from '../components/Button/Button';
+import { StyleSheet, Text, View, ImageBackground, TextInput, Alert, ActivityIndicator } from 'react-native';
 
-const propTypes = {
-    props: PropTypes.object,
-};
+
 
 class ReviewQuestion extends React.Component {
 constructor(props){
@@ -27,7 +27,7 @@ constructor(props){
          };
          console.log(data);
         axios
-      .post('https://grudlann-app.herokuapp.com/api/reviews', data, { headers: { 'Authorization': `Bearer ${JSON.parse(localStorage.getItem('token')).value}`,'Content-Type': 'application/json', 'Accept': '*/*' }})
+      .post('https://grudlann-app.herokuapp.com/api/reviews', data, { headers: { 'Authorization': `Bearer ${JSON.parse(localStorage.getItem('token'))}`,'Content-Type': 'application/json', 'Accept': '*/*' }})
       .then(res => console.log(res))
       .catch(err => console.log(err));
         };
@@ -35,16 +35,51 @@ constructor(props){
     render(){
 
     return ( 
-       this.state.hide ? (<div className={styles.wrapper}>
-            <h2 className={styles.question}>{this.props.name}</h2>
-            {this.props.answers.map(answer => (
-               <AppButton text={answer.name} value={answer.lp} onClick={this.handleClick}/>
-            ))}
-        </div>): null
+       this.state.hide ? (<View style={styles.container}>
+            <Text style={styles.text}> {this.props.name}</Text>
+            {this.props.answers.map(answer => 
+               <AppButton disabled={false} key={answer.lp} title={answer.name}  onPress={this.handleClick}/>
+        )}
+        </View>): null
     );
 };
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%'
+    },
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center",
+        width: '100%'
+    },
+    text: {
+        color: "white",
+        fontSize: 20,
+        fontWeight: "bold",
+        textAlign: "center",
+        paddingHorizontal: 20,
+        marginBottom: 30,
+        marginTop: 30,
+    },
+    input: {
+        borderColor: 'white',
+        borderWidth: 2,
+        marginHorizontal: 30,
+        color: 'white',
+        padding: 10,
+        marginTop: 20,
+    },
+    horizontal: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        padding: 10
+    }
+});
 
-ReviewQuestion.propTypes = propTypes;
 
 export default ReviewQuestion;
